255e573c6a9fe85287a8e523dfca10ef23f5e5f6
Karolina <karolina@Air-Karolina.lan>
2018-01-12 18:07:07 +0100

    Add new files.

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-function Phone(brand, price, color) {
-	this.brand = SamsungGalaxyS6;
-	this.price = 2000;
-	this.color = white;
-	this.size = 6;
+function Button(text) {
+	this.text = text || 'Hello';
 }
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-samsungGalaxyS6.printInfo();
 
-function Phone(brand, price, color) {
-	this.brand = iPhone6S;
-	this.price = 3000;
-	this.color = silver;
-	this.size = 5.5;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
 };
-iPhone6S.printInfo();
 
-function Phone(brand, price, color) {
-	this.brand = OnePlusOne;
-	this.price = 1500;
-	this.color = black;
-	this.size = 5.2;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-OnePlusOne.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create();
