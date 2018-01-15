1cdd6b6b19dd9b13a775a76643abd83a8944347a
karolinaoliwa <hethell93@gmail.com>
2018-01-15 14:32:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-function Phone(brand, price, color) {
-	this.brand = SamsungGalaxyS6;
-	this.price = 2000;
-	this.color = white;
-	this.size = 6;
+function Phone(brand, price, color,size) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.size = size;
 }
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
 };
-samsungGalaxyS6.printInfo();
 
-function Phone(brand, price, color) {
-	this.brand = iPhone6S;
-	this.price = 3000;
-	this.color = silver;
-	this.size = 5.5;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
+var iPhone6S = new Phone("Apple", 2250, "silver", 5);
+var samsungGalaxyS6 = new Phone("Samsung", 3000, "black", 6.5);
+var onePlusOne = new Phone("One plus One", 1500, "white", 5.2);
+
 iPhone6S.printInfo();
-
-function Phone(brand, price, color) {
-	this.brand = OnePlusOne;
-	this.price = 1500;
-	this.color = black;
-	this.size = 5.2;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-OnePlusOne.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
