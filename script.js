884372e83d8190c175bc4e7479e44bc14da78357
Karolina <karolina@Air-Karolina.lan>
2018-01-15 14:16:32 +0100

    Add new files.

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-function Phone(brand, price, color) {
-	this.brand = SamsungGalaxyS6;
-	this.price = 2000;
-	this.color = white;
-	this.size = 6;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-samsungGalaxyS6.printInfo();
-
-function Phone(brand, price, color) {
-	this.brand = iPhone6S;
-	this.price = 3000;
-	this.color = silver;
-	this.size = 5.5;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-iPhone6S.printInfo();
-
-function Phone(brand, price, color) {
-	this.brand = OnePlusOne;
-	this.price = 1500;
-	this.color = black;
-	this.size = 5.2;
-}
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + "the price is " + this.price + "and the size is"+ this.size + ".");
-};
-OnePlusOne.printInfo();
+$(function() {
+    
+});
